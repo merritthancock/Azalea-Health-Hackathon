9798ef71994964a7361791c75a725c4e03b2ca7a
Merritt Hancock <merrittdhancock@gmail.com>
2020-03-07 15:08:02 -0500

    ye

--- js/Manager.js
+++ js/Manager.js
@@ -8,7 +8,8 @@ function submitData() {
     let studentButton = document.getElementById("student");
     let student = studentButton.options[studentButton.selectedIndex].value;
     let bmi = getBMI(weight, height);
-    document.getElementById("bmi").innerHTML = bmi.toString();
+    let lb = document.getElementById("bmiout"); 
+    lb.textContent = bmi.toString();
 }
 
 //Function for input sanitization TODO: Fix to actually make user re-input data
